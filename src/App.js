151e7ae1acaa4651b5ab7954a5ae60ecdd8a9f1f
Abhiyan Luitel <abhiyan@mutextech.com.np>
2019-02-18 22:53:02 +0545

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Slider from 'react-animated-slider';
+import 'react-animated-slider/build/horizontal.css';
+import slider1 from './images/slider1.jpg';
+import slider2 from './images/slider2.jpg';
+import slider3 from './images/slider3.jpg';
+const sliderImages = [
+  {url: slider1, alt: "image1"},
+  {url: slider2, alt: "image2"},
+  {url: slider3, alt: "image3"},
+];
+
+const sliderImage = sliderImages.map((image,index)=>{
+  return(
+    <img
+     key={index}
+     src={image.url}
+     alt={image.alt}
+     className="sliderImage"
+     >
+     <button>click me!</button>
+     </img>
+  ) 
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App" >
+       <Slider autoplay={3500}  >
+        {sliderImage}
+       </Slider>
+        
       </div>
     );
   }
